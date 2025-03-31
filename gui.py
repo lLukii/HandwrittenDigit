7d320657236fa3f9d3e0ed6202bbbd192e8466ed
Yichen Lucas Jiao <lucas.jiao.ny@gmail.com>
2025-03-31 12:29:17 -0400

    adsfasdfasdf

--- gui.py
+++ gui.py
@@ -1,35 +1,64 @@
-import pygame, copy
+import pygame, copy, nn_model
+print("Let's make this program not dumb")
+# nn_model.train_model()
+pygame.init()
 
-screen = pygame.display.set_mode((720, 720)) 
-    
-pygame.display.set_caption('Neural Network') 
+screen = pygame.display.set_mode((720, 720))     
+pygame.display.set_caption('Smart python program!') 
 screen.fill((255, 255, 255)) 
 clock = pygame.time.Clock()
 pygame.display.flip() 
   
 running = True
-hitboxes = [[0] * 32 for _ in range(32)]
+hitboxes = [[0] * 28 for _ in range(28)]
 inputs = copy.deepcopy(hitboxes)
+font = pygame.font.Font(pygame.font.get_default_font(), 40)
 
 TILE_SIZE = 15
 TILE_X, TILE_Y = 100, 100
-for i in range(32):
-    for j in range(32):
+for i in range(28):
+    for j in range(28):
         rect = pygame.Rect(TILE_X + i*TILE_SIZE, TILE_Y + j*TILE_SIZE, TILE_SIZE, TILE_SIZE)
         hitboxes[i][j] = rect
+
+def generate_button(text, rect):
+    txt = font.render(text, True, (0,0,0))
+    hitbox = txt.get_rect()
+    hitbox.center = rect.center
+    pygame.draw.rect(screen, (255,255,255), rect)
+    screen.blit(txt, hitbox)
+
+def generate_heading(text, center):
+    title = font.render(text, True, (0,0,0))
+    titlehitbox = title.get_rect()
+    titlehitbox.center = center
+    screen.blit(title, titlehitbox)
+
 
 while running: 
     for event in pygame.event.get():     
         if event.type == pygame.QUIT: 
             running = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r: 
+                inputs = [[0] * 28 for _ in range(28)]
+                screen.fill((255,255,255))
     
     click, _, _ = pygame.mouse.get_pressed()
     mpos = pygame.mouse.get_pos()
-    for i in range(32):
-        for j in range(32):
+    for i in range(28):
+        for j in range(28):
             if hitboxes[i][j].collidepoint(mpos) and click: 
                 inputs[i][j] = 1
             pygame.draw.rect(screen, (0,0,0), hitboxes[i][j], inputs[i][j] ^ 1)
 
+    guess_b = pygame.Rect(360, 600, 50, 50)
+    generate_button("Guess the number", guess_b)
+
+    if guess_b.collidepoint(mpos) and click:
+        generate_heading(f"I think you wrote a {nn_model.test_on_handwriting(inputs)}", (360, 50))
+
     pygame.display.flip() 
-    clock.tick(24)
+    clock.tick(120)
+
---
+++ nn_model.py
@@ -0,0 +1,97 @@
+import torch
+import torch.nn as nn
+from torch import optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import numpy as np
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0,),(1,))
+])
+train_dataset = datasets.MNIST(
+    root='./data', 
+    train=True, 
+    download=True, 
+    transform=transform
+)
+test_dataset = datasets.MNIST(
+    root='./data', 
+    train=True, 
+    download=True, 
+    transform=transform
+)
+
+train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
+input_size = 784
+hidden_size = 512
+output_size = 10
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_forward = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, output_size),
+        )
+    
+    def forward(self, x):
+        x = self.flatten(x)
+        output = self.linear_relu_forward(x)
+        return output
+
+model = NeuralNetwork()
+loss_func = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+epochs = 10
+
+loss_cache = []
+def train_loop(dataloader):
+    model.train()
+    for batch, (X, Y) in enumerate(dataloader):
+        # forward prop
+        pred = model((X > 0).to(torch.float32))
+        loss = loss_func(pred, Y)
+        # backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        loss_cache.append(loss.item())
+        if batch % 1000 == 0:  
+            print(f"Current loss: {loss.item()}")
+
+def test_loop(dataloader):
+    model.eval() # sets to evaluation mode
+    size = len(dataloader.dataset)
+    correct = 0
+    with torch.no_grad():
+        for X, Y in dataloader: 
+            pred = model((X > 0).to(torch.float32))
+            correct += (pred.argmax(1) == Y).sum().item()
+
+    print(f"Accuracy: {correct / size * 100}%")
+
+def train_model():
+    for t in range(epochs):
+        print(f"Epoch #{t+1}")
+        train_loop(train_loader)
+        test_loop(test_loader)
+
+def test_on_handwriting(data):
+    data = torch.Tensor(data).reshape(1, 28, 28)
+    print(data)
+    pred = model(data)
+    return int(pred.argmax(1))
+
+
+
+
+
+
+
